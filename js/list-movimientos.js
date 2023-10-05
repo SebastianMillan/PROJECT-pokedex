4951a8751c9062d18a794a9ec6d8a82985acaf1c
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-10-05 13:16:26 +0200

    Casi terminado detalle modal falta cuadrar imagen

--- js/list-movimientos.js
+++ js/list-movimientos.js
@@ -15,19 +15,16 @@ $(document).ready(() => {
                 url: card.url,
 
             }).done(mov => {
-                var firstLetter = mov.name.split('')[0].toUpperCase();
-                var nombreMov = (firstLetter + mov.name.slice(1)).replace("-", " ");
-                console.log(mov.type.name)
-                var cardMovimiento = `<div class="col-4 movimiento mb-3">
+                var cardMovimiento = `<div class="col-4 movimiento mb-3" movId="${mov.id}">
                         <div class="card-movimiento shadow" style="border-color: ${colorBorderTipo(mov.type.name)}!important" >
                             <div class="row">
                                 <div class="col-4">
                                     <div class="img-tipo-movimiento p-1">
-                                        <img src="${getImgTipo(mov.damage_class.name)}" alt="tipo del ataque" class="w-100">
+                                        <img src="${getImgModo(mov.damage_class.name)}" alt="tipo del ataque" class="w-100">
                                     </div>
                                 </div>
                                 <div class="col-4 p-3">
-                                    <span class="fs-5 fw-bold">${nombreMov}</span><br>
+                                    <span class="fs-5 fw-bold">${formatNames(mov.name)}</span><br>
                                     <span>#<span>${mov.id}</span></span>
                                 </div>
                                 <div class="col-4 p-3">
@@ -42,6 +39,32 @@ $(document).ready(() => {
 
             });
 
+        });
+
+    });
+
+    $(document).on('click', '.movimiento', function () {
+
+        var movId = $(this).attr('movId');
+        console.log(movId)
+        console.log("asd")
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/move/${movId}`,
+            type: 'GET'
+        }).done(mov => {
+            $('#nameMov').text(formatNames(mov.name));
+            $('#idMov').text(movId);
+            $('#descMov').text(formatNames(mov.flavor_text_entries[0].flavor_text));
+            $('#tipoMov').text(formatNames(mov.type.name));
+            $('#objMov').text(formatNames(mov.target.name))
+            $('#powMov').text(mov.power != null ? mov.power : 0);
+            $('#preMov').text(mov.accurancy != null ? mov.accuracy : 0);
+            $('#ppMov').text(mov.pp != null ? mov.pp : 0);
+            $('#imgModoMov').attr("src", getImgModo(mov.damage_class.name));
+            $('#imgTipoMov').attr("src", getTipoMov(mov));
+
+
+            $('#detalleMove').modal('show');
         });
 
     });
@@ -111,7 +134,7 @@ $(document).ready(() => {
         return colorBorde;
     }
 
-    function getImgTipo(modoAtaque) {
+    function getImgModo(modoAtaque) {
         switch (modoAtaque) {
             case 'physical':
                 return "../img/ataque_fisico.PNG"
@@ -126,5 +149,96 @@ $(document).ready(() => {
                 return "0"
                 break;
         }
+    }
+
+    function formatNames(nameSinFormat) {
+        var firstLetter = nameSinFormat.split('')[0].toUpperCase();
+        return (firstLetter + nameSinFormat.slice(1)).replace("-", " ");
+    }
+
+    function getTipoMov(mov) {
+
+        var tipo = mov.type.name;
+        var foto = '';
+
+        switch (tipo) {
+
+            case 'dragon':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/7/70/Dragon_icon_SwSh.png/80px-Dragon_icon_SwSh.png';
+                break;
+
+            case 'grass':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/a/a8/Grass_icon_SwSh.png/80px-Grass_icon_SwSh.png';
+                break;
+
+            case 'fire':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/a/ab/Fire_icon_SwSh.png/80px-Fire_icon_SwSh.png';
+                break;
+
+            case 'water':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/8/80/Water_icon_SwSh.png/80px-Water_icon_SwSh.png';
+                break;
+
+            case 'poison':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/8/8d/Poison_icon_SwSh.png/80px-Poison_icon_SwSh.png';
+                break;
+
+            case 'bug':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/9/9c/Bug_icon_SwSh.png/80px-Bug_icon_SwSh.png';
+                break;
+
+            case 'ground':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/2/27/Ground_icon_SwSh.png/80px-Ground_icon_SwSh.png';
+                break;
+
+            case 'dark':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/d/d5/Dark_icon_SwSh.png/80px-Dark_icon_SwSh.png';
+                break;
+
+            case 'electric':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/7/7b/Electric_icon_SwSh.png/80px-Electric_icon_SwSh.png';
+                break;
+
+            case 'fairy':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/c/c6/Fairy_icon_SwSh.png/80px-Fairy_icon_SwSh.png';
+                break;
+
+            case 'fighting':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/3/3b/Fighting_icon_SwSh.png/80px-Fighting_icon_SwSh.png';
+                break;
+
+            case 'ghost':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/0/01/Ghost_icon_SwSh.png/80px-Ghost_icon_SwSh.png';
+                break;
+
+            case 'ice':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/1/15/Ice_icon_SwSh.png/80px-Ice_icon_SwSh.png';
+                break;
+
+            case 'normal':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/9/95/Normal_icon_SwSh.png/80px-Normal_icon_SwSh.png';
+                break;
+
+            case 'psychic':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/7/73/Psychic_icon_SwSh.png/80px-Psychic_icon_SwSh.png';
+                break;
+            case 'rock':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/1/11/Rock_icon_SwSh.png/80px-Rock_icon_SwSh.png';
+                break;
+
+            case 'steel':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/0/09/Steel_icon_SwSh.png/80px-Steel_icon_SwSh.png';
+                break;
+
+            case 'flying':
+                foto = 'https://archives.bulbagarden.net/media/upload/thumb/b/b5/Flying_icon_SwSh.png/80px-Flying_icon_SwSh.png';
+                break;
+
+            default:
+                return foto;
+                break;
+        }
+
+        return foto;
     }
 });
